116ad1dad03aac68f37460085b8f77fd12791741
unknown <saveniukf@gmail.com>
2025-02-24 16:55:03 +0200

    #1 added timeHandler func

--- main.go
+++ main.go
@@ -12,7 +12,18 @@ type Time struct {
 }
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	
+	curentTime := Time{Time: time.Now().Format(time.RFC3339)}
+	response, err := json.Marshal(curentTime)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(response)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
 
 func main() {
