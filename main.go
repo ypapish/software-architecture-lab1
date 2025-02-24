82313642f517b7e7540df5a47856f002bf7ad1d0
unknown <saveniukf@gmail.com>
2025-02-24 16:41:53 +0200

    added starting structure of project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Time struct {
+	Time string `json:"time"`
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func main() {
+	
+}
